446e4b49c88757320868ede24174bfc82a185981
Pramudya Wibisono <pramudyaw.serious@gmail.com>
2023-05-26 03:34:50 +0700

    feat: create Login Activity and layout

---
+++ app/src/main/java/com/nutriast/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.nutriast.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nutriast.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
