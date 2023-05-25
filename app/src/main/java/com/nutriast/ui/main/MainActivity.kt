ba77a57902050339176ea7be432fac521401d86a
Pramudya Wibisono <pramudyaw.serious@gmail.com>
2023-05-26 03:40:18 +0700

    chorus: add other necessities

--- app/src/main/java/com/nutriast/ui/main/MainActivity.kt
+++ app/src/main/java/com/nutriast/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.nutriast
+package com.nutriast.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nutriast.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
