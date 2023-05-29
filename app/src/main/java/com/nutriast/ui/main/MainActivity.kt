81a1b914f422729053f92f2f189877f9b83f419f
Pramudya Wibisono <pramudyaw.serious@gmail.com>
2023-05-29 16:21:39 +0700

    chorus: add temporary button in MainActivity

--- app/src/main/java/com/nutriast/ui/main/MainActivity.kt
+++ app/src/main/java/com/nutriast/ui/main/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.nutriast.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.nutriast.R
+import com.nutriast.databinding.ActivityMainBinding
+import com.nutriast.ui.login.LoginActivity
+import com.nutriast.ui.register.RegisterActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            val i = Intent(this, LoginActivity::class.java)
+            startActivity(i)
+        }
+
+        binding.registerButton.setOnClickListener {
+            val i = Intent(this, RegisterActivity::class.java)
+            startActivity(i)
+        }
     }
 }
