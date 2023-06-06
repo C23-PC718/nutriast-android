d681e1e902347f5d0d0b78ba86d1d8866b414b10
Pramudya Wibisono <pramudyaw.serious@gmail.com>
2023-06-06 10:48:19 +0700

    feat: change login layout

--- app/src/main/java/com/nutriast/ui/main/MainActivity.kt
+++ app/src/main/java/com/nutriast/ui/main/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.nutriast.ui.main
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.nutriast.databinding.ActivityMainBinding
-import com.nutriast.ui.login.LoginActivity
 import com.nutriast.ui.profile.ProfileActivity
-import com.nutriast.ui.register.RegisterActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
